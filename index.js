b079bfd3542e826a8cd60de95c41b0a9bcb768a4
RANAIVOSAMIMANANA Sarobidy <malyo20.daiki@gmail.com>
2022-11-14 16:52:50 +0300

    crud action [ok]

--- index.js
+++ index.js
@@ -2,22 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
+const booksRouter = require("./routes/books");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const PORT = process.env.PORT || 4000;
 
 const FileSync = require("lowdb/adapters/FileSync");
-
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-
 db.defaults({ books: [] }).write();
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express();
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.db = db;
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/books", booksRouter);
 
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
